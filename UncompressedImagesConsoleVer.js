40ccc3bb25fbabf6fb790af9ed88db14d6fd7abe
ニューエスト <Michael.O@LSEP-BD.jp>
2023-03-18 19:29:59 +0800

    Update UncompressedImagesConsoleVer.js

--- UncompressedImagesConsoleVer.js
+++ UncompressedImagesConsoleVer.js
@@ -1,10 +1,47 @@
-function loop(){
-		
-var images = document.querySelectorAll('img[src^="https://media.discordapp.net/attachments"]');
+	const config = {
+  attributes: true,
+  childList: true,
+  subtree: true,
+  attributeFilter: ['src'],
+};
 
-images.forEach(function(image) {
-  image.src = image.src.replace('https://media.discordapp.net/attachments', 'https://cdn.discordapp.com/attachments');
-});
+const observer = new MutationObserver(callback);
 
-    setTimeout(loop, 1000);
+function replaceURLs() {
+	
+  const images = document.querySelectorAll('img[src^="https://media.discordapp.net/attachments"]');
+  
+  images.forEach(function(image) {
+    if (!image.src.includes('.gif')) {
+      image.src = image.src.replace('https://media.discordapp.net/attachments', 'https://cdn.discordapp.com/attachments');
+    }
+  });
 }
+
+function callback(mutationsList, observer) {
+	
+  for (const mutation of mutationsList) {
+    if (mutation.type === 'childList' && mutation.addedNodes.length > 0) {
+		
+      const addedImages = mutation.target.querySelectorAll('img[src^="https://media.discordapp.net/attachments"]');
+      addedImages.forEach(function(image) {
+
+        if (!image.src.includes('.gif')) {
+          image.src = image.src.replace('https://media.discordapp.net/attachments', 'https://cdn.discordapp.com/attachments');
+        }
+      });
+	  
+    } else if (mutation.type === 'attributes' && mutation.attributeName === 'src') {
+      if (!mutation.target.src.includes('.gif')) {
+        replaceURLs();
+      }
+    }
+  }
+}
+
+function runMutation() {
+  replaceURLs();
+  observer.observe(document, config);
+}
+
+runMutation();

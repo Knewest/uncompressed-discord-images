d1c987875c206a03e353def6510f36723c0de6de
ニューエスト <Michael.O@LSEP-BD.jp>
2023-03-18 19:29:31 +0800

    Update UncompressedImages.plugin.js

--- UncompressedImages.plugin.js
+++ UncompressedImages.plugin.js
@@ -1,14 +1,14 @@
 /**
  * @name Uncompressed Images
- * @description Discord's solution to previewing images is awful so by changing 'media.discordapp.net' links to 'cdn.discordapp.com' links, we will no longer have blurry images (especially with JPEG and WebP).
  * @author Knew
+ * @description Discord's solution to previewing images is awful so by changing 'media.discordapp.net' links to 'cdn.discordapp.com' links, we will no longer have blurry images (especially with JPEG and WebP).
+ * @version 0.0.2
  * @authorId 332116671294734336
  * @authorLink https://github.com/Knewest
- * @version 0.0.1
+ * @website https://twitter.com/KnewestLSEP
  * @source https://raw.githubusercontent.com/Knewest/uncompressed-discord-images/main/UncompressedImages.plugin.js
  * @updateUrl https://raw.githubusercontent.com/Knewest/uncompressed-discord-images/main/UncompressedImages.plugin.js
  */
-
 
    module.exports = class name{
      
@@ -16,18 +16,54 @@
        start() {
 
 
-	function loop(){
-		
-var images = document.querySelectorAll('img[src^="https://media.discordapp.net/attachments"]');
+	const config = {
+  attributes: true,
+  childList: true,
+  subtree: true,
+  attributeFilter: ['src'],
+};
 
-images.forEach(function(image) {
-  image.src = image.src.replace('https://media.discordapp.net/attachments', 'https://cdn.discordapp.com/attachments');
-});
+const observer = new MutationObserver(callback);
 
-    setTimeout(loop, 1000);
+function replaceURLs() {
+	
+  const images = document.querySelectorAll('img[src^="https://media.discordapp.net/attachments"]');
+  
+  images.forEach(function(image) {
+    if (!image.src.includes('.gif')) {
+      image.src = image.src.replace('https://media.discordapp.net/attachments', 'https://cdn.discordapp.com/attachments');
+    }
+  });
 }
 
-loop();
+function callback(mutationsList, observer) {
+	
+  for (const mutation of mutationsList) {
+    if (mutation.type === 'childList' && mutation.addedNodes.length > 0) {
+		
+      const addedImages = mutation.target.querySelectorAll('img[src^="https://media.discordapp.net/attachments"]');
+      addedImages.forEach(function(image) {
+
+        if (!image.src.includes('.gif')) {
+          image.src = image.src.replace('https://media.discordapp.net/attachments', 'https://cdn.discordapp.com/attachments');
+        }
+      });
+	  
+    } else if (mutation.type === 'attributes' && mutation.attributeName === 'src') {
+      if (!mutation.target.src.includes('.gif')) {
+        replaceURLs();
+      }
+    }
+  }
+}
+
+function runMutation() {
+  replaceURLs();
+  observer.observe(document, config);
+}
+
+runMutation();
+
 }
 stop(){
   

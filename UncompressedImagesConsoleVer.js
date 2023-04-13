db84049fb266dc83152d17042cadfbddad4ba418
ニューエスト <Michael.O@LSEP-BD.jp>
2023-04-13 21:31:33 +0800

    Update UncompressedImagesConsoleVer.js to 2.2
    Version 2.2

--- UncompressedImagesConsoleVer.js
+++ UncompressedImagesConsoleVer.js
@@ -7,45 +7,34 @@ const config = {
 
 const observer = new MutationObserver(callback);
 
-function replaceURLs() {
-  const images = document.querySelectorAll('img[src^="https://media.discordapp.net/attachments"]');
-  
-  images.forEach(function(image) {
-    if (!image.src.includes('.gif')) {
-      
-      setTimeout(function() {
-        image.src = image.src.replace('https://media.discordapp.net/attachments', 'https://cdn.discordapp.com/attachments');
-      }, 750);
-    }
-  });
+function replaceURLs(image) {
+  if (!image.src.includes('.gif')) {
+    setTimeout(function() {
+      image.src = image.src.replace('https://media.discordapp.net/attachments', 'https://cdn.discordapp.com/attachments');
+    }, 125);
+  }
 }
 
 function callback(mutationsList, observer) {
   for (const mutation of mutationsList) {
     if (mutation.type === 'childList' && mutation.addedNodes.length > 0) {
-
       const addedImages = mutation.target.querySelectorAll('img[src^="https://media.discordapp.net/attachments"]');
       addedImages.forEach(function(image) {
-
-        if (!image.src.includes('.gif')) {
-          
-          setTimeout(function() {
-            image.src = image.src.replace('https://media.discordapp.net/attachments', 'https://cdn.discordapp.com/attachments');
-          }, 750);
-        }
+        replaceURLs(image);
       });
-    } 
-    
-    else if (mutation.type === 'attributes' && mutation.attributeName === 'src') {
+    } else if (mutation.type === 'attributes' && mutation.attributeName === 'src') {
       if (!mutation.target.src.includes('.gif')) {
-        replaceURLs();
+        replaceURLs(mutation.target);
       }
     }
   }
 }
 
 function runMutation() {
-  replaceURLs();
+  const images = document.querySelectorAll('img[src^="https://media.discordapp.net/attachments"]');
+  images.forEach(function(image) {
+    replaceURLs(image);
+  });
   observer.observe(document, config);
 }
 

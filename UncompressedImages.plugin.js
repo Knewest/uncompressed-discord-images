e694d3fedc4585ed9f3adca250df00d7f0ce89f1
ニューエスト <Michael.O@LSEP-BD.jp>
2023-04-13 21:30:33 +0800

    Update UncompressedImages.plugin.js to v2.2
    Version 2.2

--- UncompressedImages.plugin.js
+++ UncompressedImages.plugin.js
@@ -2,7 +2,7 @@
  * @name Uncompressed Images
  * @author Knew
  * @description Discord's solution to previewing images is awful so by changing 'media.discordapp.net' links to 'cdn.discordapp.com' links, we will no longer have blurry images (especially with JPEG and WebP).
- * @version 2.1
+ * @version 2.2
  * @authorId 332116671294734336
  * @authorLink https://github.com/Knewest
  * @website https://twitter.com/KnewestLSEP
@@ -25,45 +25,34 @@ const config = {
 
 const observer = new MutationObserver(callback);
 
-function replaceURLs() {
-  const images = document.querySelectorAll('img[src^="https://media.discordapp.net/attachments"]');
-  
-  images.forEach(function(image) {
-    if (!image.src.includes('.gif')) {
-      
-      setTimeout(function() {
-        image.src = image.src.replace('https://media.discordapp.net/attachments', 'https://cdn.discordapp.com/attachments');
-      }, 750);
-    }
-  });
+function replaceURLs(image) {
+  if (!image.src.includes('.gif')) {
+    setTimeout(function() {
+      image.src = image.src.replace('https://media.discordapp.net/attachments', 'https://cdn.discordapp.com/attachments');
+    }, 125);
+  }
 }
 
 function callback(mutationsList, observer) {
   for (const mutation of mutationsList) {
     if (mutation.type === 'childList' && mutation.addedNodes.length > 0) {
-
       const addedImages = mutation.target.querySelectorAll('img[src^="https://media.discordapp.net/attachments"]');
       addedImages.forEach(function(image) {
-
-        if (!image.src.includes('.gif')) {
-          
-          setTimeout(function() {
-            image.src = image.src.replace('https://media.discordapp.net/attachments', 'https://cdn.discordapp.com/attachments');
-          }, 750);
-        }
+        replaceURLs(image);
       });
-    } 
-    
-    else if (mutation.type === 'attributes' && mutation.attributeName === 'src') {
+    } else if (mutation.type === 'attributes' && mutation.attributeName === 'src') {
       if (!mutation.target.src.includes('.gif')) {
-        replaceURLs();
+        replaceURLs(mutation.target);
       }
     }
   }
 }
 
 function runMutation() {
-  replaceURLs();
+  const images = document.querySelectorAll('img[src^="https://media.discordapp.net/attachments"]');
+  images.forEach(function(image) {
+    replaceURLs(image);
+  });
   observer.observe(document, config);
 }
 
@@ -74,3 +63,8 @@ stop(){
   
 }
 }
+
+/**
+* Version 2.2 of Uncompressed Images
+* Copyright (Boost Software License 1.0) 2023-2023 Knew
+*/

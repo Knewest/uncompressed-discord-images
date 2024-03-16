f78dc7a27bd10e958dfa649ca2f32237f271d613
ニューエスト <Michael.O@LSEP-BD.jp>
2024-03-16 10:18:16 +0800

    Update UncompressedImagesConsoleVer.js to v3.23

--- UncompressedImagesConsoleVer.js
+++ UncompressedImagesConsoleVer.js
@@ -315,6 +315,11 @@
 	function createUncompressedImagesCSSStyle() {
 	const style = document.createElement('style');
 	style.textContent = `
+
+		.pointerCover_b7f426 {
+			z-index: -9999 !important;
+		}
+		
 		.altText__6dd8b {
 			margin: .25rem 0 -0.15rem !important;
 			line-height: 17px !important;
@@ -457,7 +462,7 @@
 	this.mutationObserver = localObserver;
 
 /**
-* Version 3.22 of 'Uncompressed Images'
+* Version 3.23 of 'Uncompressed Images'
 * Copyright (Boost Software License 1.0) 2023-2023 Knew
 * Link to plugin: https://github.com/Knewest/uncompressed-discord-images
 * Support server: https://discord.gg/NqqqzajfK4

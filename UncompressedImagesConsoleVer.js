f59e0a9a461ec9a10346bace0703829378412b18
ニューエスト <Michael.O@LSEP-BD.jp>
2024-03-16 17:20:37 +0800

    Update UncompressedImagesConsoleVer.js to v3.24 +1

--- UncompressedImagesConsoleVer.js
+++ UncompressedImagesConsoleVer.js
@@ -319,6 +319,10 @@
 		.pointerCover__98bf4 {
 			z-index: -9999 !important;
 		}
+
+		.pointerCover_b7f426 {
+			z-index: -9999 !important;
+		}
 		
 		.altText__6dd8b {
 			margin: .25rem 0 -0.15rem !important;
